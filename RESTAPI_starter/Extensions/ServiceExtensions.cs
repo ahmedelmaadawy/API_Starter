d40a67df2318a805ae16751fa87608641481c756
ahmed elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-02-18 17:48:06 +0200

    After configuration and models

---
+++ RESTAPI_starter/Extensions/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace RESTAPI_starter.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection Services) =>
+            Services.Configure<IISOptions>(options =>
+            {
+
+            });
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
---
+++ RESTAPI_starter/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using RESTAPI_starter.Extensions;
+
+namespace RESTAPI_starter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+
+            // Add services to the container.
+            builder.Services.ConfigureCors();
+            builder.Services.ConfigureIISIntegration();
+            builder.Services.ConfigureLoggerService();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello From The Middleware Component.");
+            //});
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
