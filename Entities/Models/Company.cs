93dc2499b532d2fec9218e02b21be95567fc6c2e
ahmed elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-02-18 18:55:31 +0200

    After Updating database

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Company
+    {
+        [Column("CompanyId")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Company Name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the name is 60 characters.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Company Address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Address is 60 characters.")]
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
